9f7adc97130dde98d1d8a9853ddc7b115312e37b
ZakharZverev <75448959+ZakharZverev@users.noreply.github.com>
2024-02-20 20:19:05 +0300

    Update test_table_insert.py

--- test_table_insert.py
+++ test_table_insert.py
@@ -5,7 +5,12 @@ print('Database opened')
 
 cur=con.cursor()
 
-cur.execute("INSERT INTO CLIENTS(ID,NAME,AGE,COURSE) VALUES (001,'Kolya','18', 'E-cars')")
+cur.execute("INSERT INTO CLIENTS(ID,NAME,AGE,COURSE) VALUES (001,'Kolya','19', 'E-cars')")
+cur.execute("INSERT INTO CLIENTS(ID,NAME,AGE,COURSE) VALUES (002,'Vova','19', 'Languages')")
+cur.execute("INSERT INTO CLIENTS(ID,NAME,AGE,COURSE) VALUES (003,'Olya','17', 'Physics')")
+cur.execute("INSERT INTO CLIENTS(ID,NAME,AGE,COURSE) VALUES (004,'Zakhar','18', 'E-cars')")
+cur.execute("INSERT INTO CLIENTS(ID,NAME,AGE,COURSE) VALUES (005,'Georgiy','19', 'Math')")
+cur.execute("INSERT INTO CLIENTS(ID,NAME,AGE,COURSE) VALUES (006,'Misha','19', 'IT')")
 
 con.commit()
 print('Data inserted')
